4b84fde6086363ae20e9b6d1c72001aa16bdfad1
Rick Donalson <rick.donalson@me.com>
2022-04-14 17:36:48 -0400

    Components Course Finished

--- Designing-React-Components/src/components/App.js
+++ Designing-React-Components/src/components/App.js
@@ -1,16 +1,19 @@
-import Header from "./Header";
-import Speakers from "./Speakers";
-import Layout from "./Layout";
+import Header from './Header';
+import Speakers from './Speakers';
+import Layout from './Layout';
+import { AuthProvider } from '../contexts/AuthContext';
 
 function App() {
-  return (
-    <Layout startingTheme="light">
-      <div>
-        <Header />
-        <Speakers />
-      </div>
-    </Layout>
-  );
+	return (
+		<AuthProvider initialLoggedInUser="Ronald">
+			<Layout startingTheme='light'>
+				<div>
+					<Header />
+					<Speakers />
+				</div>
+			</Layout>
+		</AuthProvider>
+	);
 }
 
 export default App;
--- Designing-React-Components/src/components/Header.js
+++ Designing-React-Components/src/components/Header.js
@@ -1,8 +1,43 @@
 import { ThemeContext } from '../contexts/ThemeContext';
-import { useContext } from "react";
+import { useContext } from 'react';
+import withAuth from './withAuth';
 
-function Header() {
+function Header({ loggedInUser, setLoggedInUser }) {
 	const { theme } = useContext(ThemeContext);
+
+	function LoggedIn({ loggedInUser, setLoggedInUser }) {
+		return (
+			<div>
+				<span>Logged in as {loggedInUser}  </span>
+				<button
+					className='btn btn-secondary'
+					onClick={() => {
+						setLoggedInUser('');
+					}}
+				>
+					Logout
+				</button>
+			</div>
+		);
+	}
+
+	function NotLoggedIn({ loggedInUser, setLoggedInUser }) {
+		return (
+			<div>
+				<button
+					className='btn btn-secondary'
+					onClick={(e) => {
+						e.preventDefault();
+						const userName = prompt('Enter Your Login Name', '');
+						setLoggedInUser(userName);
+					}}
+				>
+					Login
+				</button>
+			</div>
+		);
+	}
+
 	return (
 		<div className='padT4 padB4'>
 			<div className='d-flex justify-content-between'>
@@ -11,18 +46,23 @@ function Header() {
 				</div>
 				<div className='light'>
 					<h4 className='header-title'>Silicon Valley Code Camp</h4>
-                </div>
-                <div className={
-					theme === 'light' ? '' : 'text-info'
-				}>
-                    Hello Mr. Smith &nbsp;&nbsp;
-                    <span>
-                        <a href="#">Sign Out</a>
-                    </span>
-                </div>
+				</div>
+				<div className={theme === 'light' ? '' : 'text-info'}>
+					{loggedInUser && loggedInUser.length > 0 ? (
+						<LoggedIn
+							loggedInUser={loggedInUser}
+							setLoggedInUser={setLoggedInUser}
+						/>
+					) : (
+						<NotLoggedIn
+							loggedInUser={loggedInUser}
+							setLoggedInUser={setLoggedInUser}
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	);
 }
 
-export default Header;
+export default withAuth(Header);
