676c0a225ab92ae78fec006902b97a22bd0201f9
Rick Donalson <rick.donalson@me.com>
2023-11-13 09:11:03 -0500

    Chapt 5 Complete

--- managing-react-state/src/Header.jsx
+++ managing-react-state/src/Header.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const activeStyle = {
+  color: "purple",
+}
 
 export default function Header() {
-  return (
-    <header>
-      <nav>
-        <ul>
+	return (
+		<header>
+			<nav>
+				<ul>
+					<li>
+						<Link to='/'>
+							<img alt='Carved Rock Fitness' src='/images/logo.png' />
+						</Link>
+          </li>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <NavLink activeStyle={activeStyle} to='/shoes' >Shoes</NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to='/cart' >Cart</NavLink>
           </li>
         </ul>
-      </nav>
-    </header>
-  );
+			</nav>
+		</header>
+	);
 }
