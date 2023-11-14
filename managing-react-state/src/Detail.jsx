2da941caa11a1ef666de9f86797a8168a6f4b928
Rick Donalson <rick.donalson@me.com>
2023-11-14 13:44:32 -0500

    Chapt 5 Complete

--- managing-react-state/src/Detail.jsx
+++ managing-react-state/src/Detail.jsx
@@ -1,32 +1,47 @@
-import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import useFetch from "./services/useFetch";
-import Spinner from "./Spinner";
-import PageNotFound from "./PageNotFound";
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import useFetch from './services/useFetch';
+import Spinner from './Spinner';
+import PageNotFound from './PageNotFound';
 
-export default function Detail() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const { data: product, loading, error } = useFetch(`products/${id}`);
+export default function Detail(props) {
+	const { id } = useParams();
+	const navigate = useNavigate();
+	const [sku, setSku] = useState('');
+	const { data: product, loading, error } = useFetch(`products/${id}`);
 
 	if (loading) return <Spinner />;
 	if (!product) return <PageNotFound />;
-  if (error) throw error;
+	if (error) throw error;
 
-  return (
-    <div id="detail">
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p id="price">${product.price}</p>
-      <p>
-        <button
-          className="btn btn-primary"
-          onClick={() => navigate("/cart")}
-        >
-          Add To Cart
-        </button>
-      </p>
-      <img src={`/images/${product.image}`} alt={product.category} />
-    </div>
-  );
+	return (
+		<div id='detail'>
+			<h1>{product.name}</h1>
+			<p>{product.description}</p>
+			<p id='price'>${product.price}</p>
+
+			<select id='size' value={sku} onChange={(e) => setSku(e.target.value)}>
+				<option value=''>What size?</option>
+				{product.skus.map((s) => (
+					<option key={s.sku} value={s.sku}>
+						{s.size}
+					</option>
+				))}
+			</select>
+
+			<p>
+				<button
+					disabled={!sku}
+					className='btn btn-primary'
+					onClick={() => {
+						props.addToCart(id, sku);
+						navigate('/cart');
+					}}
+				>
+					Add To Cart
+				</button>
+			</p>
+			<img src={`/images/${product.image}`} alt={product.category} />
+		</div>
+	);
 }
