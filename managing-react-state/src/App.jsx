5acba9bfc13376a898a551a4f996c917da1d7317
Rick Donalson <rick.donalson@me.com>
2023-11-15 15:58:28 -0500

    Chapt 6

--- managing-react-state/src/App.jsx
+++ managing-react-state/src/App.jsx
@@ -6,28 +6,29 @@ import Products from './Products';
 import { Routes, Route } from 'react-router-dom';
 import Detail from './Detail';
 import Cart from './Cart';
+import Checkout from './Checkout';
 
 export default function App() {
 	const [cart, setCart] = useState(() => {
 		try {
-			return JSON.parse(localStorage.getItem("cart")) ?? [];
+			return JSON.parse(localStorage.getItem('cart')) ?? [];
 		} catch {
-			console.error("The cart could not be parsed into JSON");
+			console.error('The cart could not be parsed into JSON');
 			return [];
 		}
 	});
 
-	useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+	useEffect(() => localStorage.setItem('cart', JSON.stringify(cart)), [cart]);
 
 	function addToCart(id, sku) {
 		setCart((items) => {
 			const itmeInCart = items.find((i) => i.sku === sku);
 			if (itmeInCart) {
 				// Return new array with the matching item replaced
-				return items.map((i) => i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i);
+				return items.map((i) => (i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i));
 			} else {
 				// Return new array with the new item appended
-				return [...items, { id, sku, quantity: 1 }] // Same as { id: id, sku: sku, quantity: 1 }
+				return [...items, { id, sku, quantity: 1 }]; // Same as { id: id, sku: sku, quantity: 1 }
 			}
 		});
 	}
@@ -36,8 +37,12 @@ export default function App() {
 		setCart((items) => {
 			return quantity === 0
 				? items.filter((i) => i.sku !== sku)
-				: items.map((i) => i.sku === sku ? { ...i, quantity } : i)
-		})
+				: items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
+		});
+	}
+
+	function emptyCart() {
+		setCart([]);
 	}
 
 	return (
@@ -48,11 +53,9 @@ export default function App() {
 					<Routes>
 						<Route path='/' element={<h1>Welcome to Carved Rock Fitness</h1>} />
 						<Route path='/:category' element={<Products />} />
-						<Route 
-							path='/:category/:id'
-							element={<Detail addToCart={addToCart} />}
-						/>
+						<Route path='/:category/:id' element={<Detail addToCart={addToCart} />} />
 						<Route path='/cart' element={<Cart cart={cart} updateQuantity={updateQuantity} />} />
+						<Route path='/checkout' element={<Checkout cart={cart} emptyCart={emptyCart} />} />
 					</Routes>
 				</main>
 			</div>
