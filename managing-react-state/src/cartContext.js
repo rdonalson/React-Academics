85d51b47921e528bc12928a36201c1ce216de1bb
Rick Donalson <rick.donalson@me.com>
2023-11-21 17:04:17 -0500

    Chapt 10

--- managing-react-state/src/cartContext.js
+++ managing-react-state/src/cartContext.js
@@ -1,36 +1,36 @@
-import React, { useReducer, useEffect, useContext } from 'react';
-import cartReducer from './cartReducer';
+import React, { useReducer, useEffect, useContext } from "react";
+import cartReducer from "./cartReducer";
 
-const CartContext = React.createContext(null);
+export const CartContext = React.createContext(null);
 
 let initialCart;
 try {
-    initialCart = JSON.parse(localStorage.getItem('cart')) ?? [];
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
 } catch {
-    console.error('The cart could not be parsed into JSON');
-    initialCart = [];
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
 }
 
 export function CartProvider(props) {
-    const [cart, dispatch] = useReducer(cartReducer, initialCart);
-    useEffect(() => localStorage.setItem('cart', JSON.stringify(cart)), [cart]);
-    const contextValue = {
-        cart,
-        dispatch,
-    };
-    return (
-        <CartContext.Provider value={contextValue}>
-            {props.children}
-        </CartContext.Provider>
-    );
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+  const contextValue = {
+    cart,
+    dispatch,
+  };
+  return (
+    <CartContext.Provider value={contextValue}>
+      {props.children}
+    </CartContext.Provider>
+  );
 }
 
 export function useCart() {
-    const context = useContext(CartContext);
-    if (!context) {
-        throw new Error(
-            "useCart must be used within a CartProvider. Wrap a parent component in <CartProvider> to fix this error."
-        );
-    }
-    return context;
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error(
+      "useCart must be used within a CartProvider. Wrap a parent component in <CartProvider> to fix this error."
+    );
+  }
+  return context;
 }
